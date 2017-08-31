f8e575f670638728672bd5b68e9ed4370591841f
Maren Sigson <msigson@gmail.com>
2017-08-31 19:18:45 -0400

    deleted autocompleted attempted code.  Pulling intot upstream master all files including amendents to gulp w/ gulp plumber

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -276,38 +276,11 @@ app.scrollToLanding = function(){
 
 // Maren
 //  - initialize the auto complete library
-// app.initLocationInput = function () {
-// 	app.initAutocomplete()
+app.initLocationInput = function () {
+	app.initAutocomplete()
 	
-// };
+};
 
-// app.initAutocomplete = function() {
-// 	// from google developer site - https://developers.google.com/maps/documentation/javascript/examples/places-searchbox
-
-//         // Create the search box and link it to the UI element.
-//         var input = document.getElementById('pac-input');
-//         var searchBox = new google.maps.places.SearchBox(input);
-//         map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-//         // Listen for the event fired when the user selects a prediction and retrieve
-//         // more details for that place.
-//         searchBox.addListener('places_changed', function() {
-//           var places = searchBox.getPlaces();
-
-//           if (places.length == 0) {
-//             return;
-//           }
-
-//           // For each place, get the icon, name and location.
-//           var bounds = new google.maps.LatLngBounds();
-//           places.forEach(function(place) {
-//             if (!place.geometry) {
-//               console.log("Returned place contains no geometry");
-//               return;
-//             }
-//           });
-//         });
-// }
 
 /********** Spotify API Related Functions ***********/
 
--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,33 +1,3 @@
-	// function initAutocomplete() {
 
-	//   // Create the search box and link it to the UI element.
-	//   var input = document.getElementById('pac-input');
-	//   var searchBox = new google.maps.places.SearchBox(input);
-	//   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-	//   // Listen for the event fired when the user selects a prediction and retrieve
-	//   // more details for that place.
-	//   searchBox.addListener('places_changed', function() {
-	//     var places = searchBox.getPlaces();
-
-	//     if (places.length == 0) {
-	//       return;
-	//     }
-
-	//     // For each place, get the icon, name and location.
-	//     var bounds = new google.maps.LatLngBounds();
-	//     places.forEach(function(place) {
-	//       if (!place.geometry) {
-	//         console.log("Returned place contains no geometry");
-	//         return;
-	//       }
-	//     });
-	//   });
-	// }
 $( app.init() );
 
-// $(function(){
-// 	app.init();
-// 	initAutocomplete()
-
-// });
